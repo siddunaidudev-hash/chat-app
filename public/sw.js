87cd87b6440f2a085574fa22d8997547f58c08eb
Siddu Naidu <siddunaidudev@gmail.com>
2026-09-04 15:16:10 +0530

    Fix sw.js and render URL

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,36 @@
-self.addEventListener('fetch', event => {});
+const CACHE_NAME = 'priconkt-v1';
+const urlsToCache = [
+  '/',
+  '/css/style.css',
+  '/js/chat.js',
+  '/js/call.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('/api/') ||
+      event.request.url.includes('socket.io')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
